1a511c931177578a0e3d167eb6fbf7773a4ba58e
EmmanuelArenas <emmanuel.live19@gmail.com>
2022-08-17 23:48:51 -0500

    Actualice diseño y componente
    Cree checkbox de terminos y condiciones, msj de error, boton enviar, msj de exito y los estilos de cada elelemento

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import React from "react";
-import { Formulario, Label, GrupoInput, Input } from "./style/formularios.js";
+import {
+  Formulario,
+  Label,
+  GrupoInput,
+  Input,
+  IconValidacion,
+  LeyendaError,
+  ContTerminos,
+  ContBoton,
+  Boton,
+  MsjExito,
+  MsjError,
+} from "./style/formularios.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheckCircle,
+  faExclamationTriangle,
+} from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
   return (
     <main>
       <Formulario action="">
         <div>
-          <Label htmlFor="">Usuario</Label>
+          <Label htmlFor="nombre">Usuario</Label>
           <GrupoInput>
-            <Input type="text" placeholder="Usuario" />
-            <FontAwesomeIcon icon={faCheckCircle} />
+            <Input type="text" placeholder="Usuario" id="nombre" />
+            <IconValidacion icon={faCheckCircle} />
           </GrupoInput>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <LeyendaError>Lorem ipsum dolor sit amet.</LeyendaError>
         </div>
+        <ContTerminos>
+          <Label>
+            <input type="checkbox" name="terminos" id="terminos" /> Acepto los
+            Terminos y Condiciones
+          </Label>
+        </ContTerminos>
+        {false && (
+          <MsjError>
+            <p>
+              <FontAwesomeIcon icon={faExclamationTriangle} />
+              <b>Error</b>Llena el formulario correctamente
+            </p>
+          </MsjError>
+        )}
+        <ContBoton>
+          <Boton type="submit">Enviar</Boton>
+          <MsjExito>Formulado enviado correctamente</MsjExito>
+        </ContBoton>
       </Formulario>
     </main>
   );
--- src/style/formularios.js
+++ src/style/formularios.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const colores = {
   borde: "#79B8D1",
   error: "#F47C7C",
-  exito: "#AEF4A4",
+  exito: "#6dcc60",
 };
 
 const Formulario = styled.form`
@@ -45,4 +46,85 @@ const Input = styled.input`
   }
 `;
 
-export { Formulario, Label, GrupoInput, Input };
+const LeyendaError = styled.p`
+  font-size: 12px;
+  margin-bottom: 0;
+  color: ${colores.error};
+  display: none;
+`;
+
+const IconValidacion = styled(FontAwesomeIcon)`
+  position: absolute;
+  right: 10px;
+  bottom: 14px;
+  z-index: 100;
+  font-size: 16px;
+  opacity: 0;
+`;
+
+const ContTerminos = styled.div`
+  grid-column: span 2;
+  input {
+    margin-right: 10px;
+  }
+`;
+
+const ContBoton = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  grid-column: span 2;
+`;
+
+const Boton = styled.button`
+  height: 45px;
+  line-height: 45px;
+  width: 30%;
+  background: #000;
+  color: #fff;
+  font-weight: bold;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  transition: 0.1s ease all;
+
+  &:hover {
+    box-shadow: 3px 0px 30px rgba(163, 163, 163, 1);
+  }
+`;
+
+const MsjExito = styled.p`
+  font-size: 14px;
+  color: ${colores.exito};
+  display: none;
+`;
+
+const MsjError = styled.div`
+  height: 45px;
+  line-height: 45px;
+  background: #f66060;
+  padding: 0px 15px;
+  border-radius: 3px;
+  grid-column: span 2;
+  p {
+    margin: 0;
+  }
+
+  b {
+    margin-left: 10px;
+  }
+`;
+
+export {
+  Formulario,
+  Label,
+  GrupoInput,
+  Input,
+  IconValidacion,
+  LeyendaError,
+  ContTerminos,
+  ContBoton,
+  Boton,
+  MsjExito,
+  MsjError,
+};
